d969b05217fdafe3de5b3db60ce85495b31d2072
SarahLightBourne <SaraLightBourne@gmail.com>
2022-01-11 14:39:17 +0200

    Update hypervisor.py

--- blog_parser/hypervisor.py
+++ blog_parser/hypervisor.py
@@ -1,11 +1,13 @@
+from .parsers import Parser
 from .requests_session import RequestsSession
-from typing import Sequence, AnyStr, Tuple
+
+from typing import Sequence, AnyStr, Tuple, List
 
 
 class HyperVisor:
   ''' a Class for hypervisoring all parser modules '''
 
-  def __init__(self, keywords: Sequence[AnyStr], *parser_classes: Tuple[object]) -> None:
+  def __init__(self, keywords: Sequence[AnyStr], *parser_classes: Tuple[Parser]) -> None:
     self.parser_classes, self.parsers = parser_classes, None
     self.requests_session_class = RequestsSession()
     self.keywords = keywords
@@ -13,6 +15,6 @@ class HyperVisor:
   async def initialize(self) -> None:
     requests_session = await self.requests_session_class.get_requests_session()
 
-    self.parsers = [
+    self.parsers: List[Parser] = [
       parser_class(requests_session, self.keywords)
     for parser_class in self.parser_classes]
