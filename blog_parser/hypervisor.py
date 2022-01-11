85973348ed181586e80f938a2bb943072a6efd0b
SarahLightBourne <SaraLightBourne@gmail.com>
2022-01-11 15:15:26 +0200

    Update hypervisor.py

--- blog_parser/hypervisor.py
+++ blog_parser/hypervisor.py
@@ -1,7 +1,9 @@
 from .parsers import Parser
 from .requests_session import RequestsSession
 
-from typing import Sequence, AnyStr, Tuple, List
+import asyncio
+from functools import reduce
+from typing import Sequence, AnyStr, Tuple, List, Dict
 
 
 class HyperVisor:
@@ -18,3 +20,24 @@ class HyperVisor:
     self.parsers: List[Parser] = [
       parser_class(requests_session, self.keywords)
     for parser_class in self.parser_classes]
+
+  async def parse(self, count: int) -> List[Dict[str, str]]:
+    load = self.__get_load(count, len(self.parsers))
+
+    results = await asyncio.gather(*[
+      parser_class.parse(its_load)
+    for parser_class, its_load in zip(self.parsers, load)])
+
+    return list(reduce(lambda a, x: a + x, results))
+
+  def __get_load(self, total: int, count: int) -> List[int]:
+    devided = total // count
+    load = [devided] * count
+
+    if total % count != 0:
+      counter = -1
+
+      while sum(load) != total:
+        load[(counter := counter + 1)] += 1
+
+    return load
