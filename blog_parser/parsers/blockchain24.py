1eb24ba4bb0df1473b528ea6aa149bf1300db2a0
SarahLightBourne <SaraLightBourne@gmail.com>
2022-01-11 15:58:44 +0200

    Update blockchain24.py

--- blog_parser/parsers/blockchain24.py
+++ blog_parser/parsers/blockchain24.py
@@ -1,12 +1,62 @@
 from .parser_base import Parser
-from typing import List, Dict
+from ..utilities import sync_to_async
+
+import asyncio
+from bs4 import BeautifulSoup
+from typing import List, Tuple, Dict
 
 
 class BlockChain24(Parser):
   ''' blockchain24.pro module '''
 
-  BASE_URL = 'https://blockchain24.pro/'
+  BASE_URL = 'https://blockchain24.pro'
   NEWS_URL = 'https://blockchain24.pro/ajax/more_articles.php?category=3387'
 
   async def parse(self, count: int) -> List[Dict[str, str]]:
-    pass
+    articles_url, offset = list(), 1
+
+    while len(articles_url) < count:
+      response = await self.requests_session.get(self.NEWS_URL, params={'start_from': offset})
+      html_code = response.text
+
+      articles, articles_count = await self.__parse_articles(html_code)
+      articles_url = [*articles_url, *articles]
+      offset += articles_count
+
+    result = await asyncio.gather(*[
+      self.parse_article(article_url)
+    for article_url in articles_url[:count]])
+
+    return result
+
+  async def parse_article(self, url: str) -> Dict[str, str]:
+    response = await self.requests_session.get(url)
+    html_code = response.text
+    return await self.__parse_article(html_code)  
+
+  @sync_to_async
+  def __parse_articles(self, html_code: str) -> Tuple[List[str], int]:
+    parser = BeautifulSoup(html_code, 'html.parser')
+    articles = parser.find_all(class_='articles__block-item')
+    articles_count = len(articles)
+
+    result = [
+      self.BASE_URL + article.find(class_='articles__block-item--header').get('href')
+    for article in articles if self.__check_keywords(article.find(class_='articles__block-item--header').text, article.find(class_='article-short-info').text)]
+
+    return result, articles_count
+
+  @sync_to_async
+  def __parse_article(self, html_code: str) -> Dict[str, str]:
+    result = dict()
+    parser = BeautifulSoup(html_code, 'html.parser')
+
+    result['language'] = parser.find('html').get('lang')
+    result['title'] = parser.find(class_='article__header').text
+    result['text'] = parser.find(class_='article__content').text.strip()
+    result['date'] = parser.find(class_='article__info-date').text.strip()
+
+    return result
+
+  def __check_keywords(self, *texts: Tuple[str]) -> bool:
+    return any([any(map(lambda keyword: keyword in text.lower(), self.keywords)) for text in texts])
