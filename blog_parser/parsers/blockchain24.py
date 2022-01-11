91ad7d1b462e0193f846efbe579c89ad2014b58f
SarahLightBourne <SaraLightBourne@gmail.com>
2022-01-11 14:17:45 +0200

    Update blockchain24.py

--- blog_parser/parsers/blockchain24.py
+++ blog_parser/parsers/blockchain24.py
@@ -1,8 +1,10 @@
+from typing import Sequence, AnyStr
 
 
 class BlockChain24:
   BASE_URL = 'https://blockchain24.pro/'
   NEWS_URL = 'https://blockchain24.pro/ajax/more_articles.php?category=3387'
 
-  def __init__(self, requests_session) -> None:
+  def __init__(self, requests_session, keywords: Sequence[AnyStr]) -> None:
       self.requests_session = requests_session
+      self.keywords = keywords
