211c0fd3a040c9807264edfd59c33eb125dbec95
SarahLightBourne <SaraLightBourne@gmail.com>
2022-02-02 22:56:49 +0300

    Update requests_session.py

--- blog_parser/requests_session.py
+++ blog_parser/requests_session.py
@@ -9,7 +9,7 @@ class RequestsSession:
     self.requests_session: AsyncClient = None
 
   async def get_requests_session(self) -> AsyncClient:
-    self.requests_session = self.requests_session or AsyncClient(headers=Headers().generate())
+    self.requests_session = self.requests_session or AsyncClient(headers=Headers().generate(), timeout=5000)
     return self.requests_session
 
   async def close_requests_session(self) -> None:
